5071fddefb0e88b830a06bbde79a8fbb3b0ab9d2
ArditZubaku <zubaku92@gmail.com>
2023-08-30 07:17:18 +0200

    LoginWindow view and controller.

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module JAVAFX.PROJEKTI {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.web;
+
+    opens com.zubaku;
+    opens com.zubaku.view;
+
+}
