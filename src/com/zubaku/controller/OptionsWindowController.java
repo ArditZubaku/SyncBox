f812514335c807541995a0673010c9a45a20e4c7
ArditZubaku <zubaku92@gmail.com>
2023-08-31 07:34:46 +0200

    feat: Initializing font size picker values.

--- src/com/zubaku/controller/OptionsWindowController.java
+++ src/com/zubaku/controller/OptionsWindowController.java
@@ -3,11 +3,13 @@ package com.zubaku.controller;
 import com.zubaku.processors.EmailProcessor;
 import com.zubaku.processors.ViewProcessor;
 import com.zubaku.utils.ColorTheme;
+import com.zubaku.utils.FontSize;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Slider;
+import javafx.util.StringConverter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -38,11 +40,38 @@ public class OptionsWindowController extends BaseController implements Initializ
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setupThemePicker();
+        setupSizePicker();
     }
 
     private void setupThemePicker() {
         themePicker.setItems(FXCollections.observableArrayList(ColorTheme.values()));
         // Default value:
         themePicker.setValue(viewProcessor.getColorTheme());
+    }
+
+    private void setupSizePicker() {
+        fontSizePicker.setMin(0);
+        fontSizePicker.setMax(FontSize.values().length - 1);
+        fontSizePicker.setValue(viewProcessor.getFontSize().ordinal());
+        fontSizePicker.setMinorTickCount(0);
+        fontSizePicker.setMajorTickUnit(1);
+        fontSizePicker.setBlockIncrement(1);
+        fontSizePicker.setSnapToTicks(true);
+        fontSizePicker.setShowTickMarks(true);
+        fontSizePicker.setShowTickLabels(true);
+        // Showing enums values instead of numbers
+        fontSizePicker.setLabelFormatter(new StringConverter<>() {
+            @Override
+            public String toString(Double object) {
+                // Initializing the value
+                int i = object.intValue();
+                return FontSize.values()[i].toString();
+            }
+
+            @Override
+            public Double fromString(String string) {
+                return null;
+            }
+        });
     }
 }
