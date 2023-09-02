48e90c01001b23ca74d2b43ebdd22b7e8c92c64e
ArditZubaku <zubaku92@gmail.com>
2023-09-02 10:28:03 +0200

    feat: initializing email field and password field.

--- src/com/zubaku/controllers/LoginWindowController.java
+++ src/com/zubaku/controllers/LoginWindowController.java
@@ -4,15 +4,19 @@ import com.zubaku.models.EmailAccount;
 import com.zubaku.processors.EmailProcessor;
 import com.zubaku.processors.ViewProcessor;
 import com.zubaku.services.LoginService;
+import com.zubaku.utils.Credentials;
 import com.zubaku.utils.LoginResult;
-
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class LoginWindowController extends BaseController {
+public class LoginWindowController
+        extends BaseController implements Initializable {
 
     @FXML
     private TextField emailAddressField;
@@ -23,16 +27,26 @@ public class LoginWindowController extends BaseController {
     @FXML
     private PasswordField passwordField;
 
-    public LoginWindowController(EmailProcessor emailProcessor, ViewProcessor viewProcessor, String fxmlName) {
+    public LoginWindowController(EmailProcessor emailProcessor,
+            ViewProcessor viewProcessor, String fxmlName) {
         super(emailProcessor, viewProcessor, fxmlName);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        emailAddressField.setText(Credentials.EMAIL_ADDRESS());
+        passwordField.setText(Credentials.PASSWORD());
     }
 
     @FXML
     void loginButtonAction() {
         if (validFields()) {
-            EmailAccount account = new EmailAccount(emailAddressField.getText(), passwordField.getText());
+            EmailAccount account = new EmailAccount(emailAddressField.getText(),
+                    passwordField.getText());
             LoginService service = new LoginService(account, emailProcessor);
+            // Start will do all the background tasks
             service.start();
+            // After finishing the tasks
             service.setOnSucceeded(event -> {
                 LoginResult result = service.getValue();
                 switch (result) {
@@ -43,6 +57,9 @@ public class LoginWindowController extends BaseController {
                         viewProcessor.closeStage(stage);
                         return;
                     }
+                    case FAILED_BY_CREDENTIALS -> errorLabel.setText("Invalid Credentials");
+                    case FAILED_BY_NETWORK -> errorLabel.setText("Check Your Network Connection");
+                    case FAILED_BY_UNEXPECTED_ERROR -> errorLabel.setText("Unexpected Error");
                     default -> throw new IllegalArgumentException("Unexpected value: " + result);
                 }
             });
---
+++ src/com/zubaku/utils/Credentials.java
@@ -0,0 +1,21 @@
+package com.zubaku.utils;
+
+public enum Credentials {
+    EMAIL_ADDRESS("zubaku92@gmail.com"),
+    PASSWORD("gwjdxxfrivgujjzt");
+
+    public static String EMAIL_ADDRESS() {
+        return Credentials.EMAIL_ADDRESS.credential;
+    }
+
+    public static String PASSWORD() {
+        return Credentials.PASSWORD.credential;
+    }
+
+    private final String credential;
+
+    Credentials(String credential) {
+        this.credential = credential;
+    }
+
+}
