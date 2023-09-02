3e3c6cee6e17b254a744d619b71162ca3187b147
ArditZubaku <zubaku92@gmail.com>
2023-09-03 00:25:42 +0200

    feat: Added addEmailAccount for multiple accounts usage.

--- src/com/zubaku/processors/EmailProcessor.java
+++ src/com/zubaku/processors/EmailProcessor.java
@@ -1,5 +1,22 @@
 package com.zubaku.processors;
 
+import com.zubaku.models.EmailAccount;
+import javafx.scene.control.TreeItem;
+
 public class EmailProcessor {
   // To manage email actions
+  // Folder handling
+
+  private TreeItem<String> foldersRoot = new TreeItem<String>("");
+
+  public TreeItem<String> getFoldersRoot() { return foldersRoot; }
+
+  public void addEmailAccount(EmailAccount account) {
+    TreeItem<String> treeItem = new TreeItem<String>(account.getEmail());
+    treeItem.setExpanded(true);
+    treeItem.getChildren().add(new TreeItem<String>("INBOX"));
+    treeItem.getChildren().add(new TreeItem<String>("SENT"));
+    treeItem.getChildren().add(new TreeItem<String>("SPAM"));
+    foldersRoot.getChildren().add(treeItem);
+  }
 }
