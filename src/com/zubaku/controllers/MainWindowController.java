6b259d37e01b4865a349ceffbb0b4ff932d0ecd4
ArditZubaku <zubaku92@gmail.com>
2023-09-03 15:37:28 +0200

    feat: Made unread emails appear bold.

--- src/com/zubaku/controllers/MainWindowController.java
+++ src/com/zubaku/controllers/MainWindowController.java
@@ -10,10 +10,12 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TreeView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.web.WebView;
+import javafx.util.Callback;
 
 public class MainWindowController
     extends BaseController implements Initializable {
@@ -39,6 +41,7 @@ public class MainWindowController
     setUpEmailsTreeView();
     setUpEmailsTableView();
     setUpSelectedFolder();
+    setUpUnreadEmailsAsBold();
   }
 
   @FXML
@@ -74,6 +77,27 @@ public class MainWindowController
       // Checking for null pointer exception
       if (treeItem != null) {
         emailsTableView.setItems(treeItem.getEmailMessages());
+      }
+    });
+  }
+
+  private void setUpUnreadEmailsAsBold() {
+    emailsTableView.setRowFactory(new Callback<>() {
+      @Override
+      public TableRow<EmailMessage> call(TableView<EmailMessage> param) {
+        return new TableRow<>() {
+          @Override
+          protected void updateItem(EmailMessage item, boolean empty) {
+            super.updateItem(item, empty);
+            if (item != null) {
+              if (item.isRead()) {
+                setStyle("");
+              } else {
+                setStyle("-fx-font-weight: bold");
+              }
+            }
+          }
+        };
       }
     });
   }
