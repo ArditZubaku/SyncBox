e2fb1c0e7d517002ff6df974dcd67e26c6c3a164
ArditZubaku <zubaku92@gmail.com>
2023-10-22 23:54:10 +0200

    feat: Downloading attachments and saving them into Downloads folder.

--- src/com/zubaku/controllers/EmailDetailsController.java
+++ src/com/zubaku/controllers/EmailDetailsController.java
@@ -4,15 +4,26 @@ import com.zubaku.models.EmailMessage;
 import com.zubaku.processors.EmailProcessor;
 import com.zubaku.processors.ViewProcessor;
 import com.zubaku.services.MessageRendererService;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.web.WebView;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeBodyPart;
 
 public class EmailDetailsController extends BaseController implements Initializable {
+  private static final Logger LOGGER = Logger.getLogger(EmailDetailsController.class.getName());
+  private final String DOWNLOADS_FOLDER = System.getProperty("user.home") + "/Downloads/SyncBox/";
+
   @FXML private Label attachmentsLabel;
 
   @FXML private HBox downloadsHBox;
@@ -28,7 +39,6 @@ public class EmailDetailsController extends BaseController implements Initializa
     super(emailProcessor, viewProcessor, fxmlName);
   }
 
-
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     EmailMessage emailMessage = emailProcessor.getSelectedEmailMessage();
@@ -36,8 +46,61 @@ public class EmailDetailsController extends BaseController implements Initializa
     subjectLabel.setText(emailMessage.getSubject());
     senderLabel.setText(emailMessage.getSender());
 
+    loadAttachments(emailMessage);
+
     MessageRendererService service = new MessageRendererService(webView.getEngine());
     service.setEmailMessage(emailMessage);
     service.restart();
+  }
+
+  private void mkDirIfNotExists() {
+    File file = new File(DOWNLOADS_FOLDER);
+    if (!file.exists()) file.mkdir();
+  }
+
+  private void loadAttachments(EmailMessage emailMessage) {
+    if (emailMessage.hasAttachments()) {
+      for (MimeBodyPart mimeBodyPart : emailMessage.getAttachmentsList()) {
+        try {
+          AttachmentButton button = new AttachmentButton(mimeBodyPart);
+          downloadsHBox.getChildren().add(button);
+        } catch (MessagingException e) {
+          LOGGER.log(Level.SEVERE, "Error loading attachments");
+        }
+      }
+    } else {
+      attachmentsLabel.setText("");
+    }
+  }
+
+  private class AttachmentButton extends Button {
+    private final MimeBodyPart mimeBodyPart;
+    private final String downloadedFilePath;
+
+    public AttachmentButton(MimeBodyPart mimeBodyPart) throws MessagingException {
+      this.mimeBodyPart = mimeBodyPart;
+      this.setText(mimeBodyPart.getFileName());
+      this.downloadedFilePath = DOWNLOADS_FOLDER + mimeBodyPart.getFileName();
+      this.setOnAction(event -> downloadAttachment());
+    }
+
+    private void downloadAttachment() {
+      // Since this might be a long going task, we need to put it in a separate thread
+      Service<Void> service =
+          new Service<>() {
+            @Override
+            protected Task<Void> createTask() {
+              return new Task<>() {
+                @Override
+                protected Void call() throws Exception {
+                  mkDirIfNotExists();
+                  mimeBodyPart.saveFile(downloadedFilePath);
+                  return null;
+                }
+              };
+            }
+          };
+      service.restart();
+    }
   }
 }
--- src/com/zubaku/models/EmailMessage.java
+++ src/com/zubaku/models/EmailMessage.java
@@ -3,11 +3,9 @@ package com.zubaku.models;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javax.mail.Message;
-import javax.mail.MessagingException;
 import javax.mail.internet.MimeBodyPart;
 
 public class EmailMessage {
@@ -17,13 +15,19 @@ public class EmailMessage {
   private final SimpleStringProperty recipient;
   private final SimpleObjectProperty<EmailSize> size;
   private final SimpleObjectProperty<Date> date;
-  private boolean isRead;
   private final Message message;
   private final List<MimeBodyPart> attachmentsList = new ArrayList<>();
+  private boolean isRead;
   private boolean hasAttachments = false;
 
-  public EmailMessage(String subject, String sender, String recipient, int size,
-                      Date date, boolean isRead, Message message) {
+  public EmailMessage(
+      String subject,
+      String sender,
+      String recipient,
+      int size,
+      Date date,
+      boolean isRead,
+      Message message) {
     this.subject = new SimpleStringProperty(subject);
     this.sender = new SimpleStringProperty(sender);
     this.recipient = new SimpleStringProperty(recipient);
@@ -33,21 +37,45 @@ public class EmailMessage {
     this.message = message;
   }
 
-  public String getSubject() { return this.subject.get(); }
+  public List<MimeBodyPart> getAttachmentsList() {
+    return attachmentsList;
+  }
 
-  public String getSender() { return this.sender.get(); }
+  public boolean hasAttachments() {
+    return hasAttachments;
+  }
 
-  public String getRecipient() { return this.recipient.get(); }
+  public String getSubject() {
+    return this.subject.get();
+  }
 
-  public EmailSize getSize() { return this.size.get(); }
+  public String getSender() {
+    return this.sender.get();
+  }
 
-  public Date getDate() { return this.date.get(); }
+  public String getRecipient() {
+    return this.recipient.get();
+  }
 
-  public boolean isRead() { return this.isRead; }
+  public EmailSize getSize() {
+    return this.size.get();
+  }
 
-  public void setRead(boolean read) { this.isRead = read; }
+  public Date getDate() {
+    return this.date.get();
+  }
 
-  public Message getMessage() { return this.message; }
+  public boolean isRead() {
+    return this.isRead;
+  }
+
+  public void setRead(boolean read) {
+    this.isRead = read;
+  }
+
+  public Message getMessage() {
+    return this.message;
+  }
 
   public void addAttachment(MimeBodyPart mimeBodyPart) {
     hasAttachments = true;
