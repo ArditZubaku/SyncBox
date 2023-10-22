af338119500af6f0cd38017f4d3f7766b7d459ac
ArditZubaku <zubaku92@gmail.com>
2023-10-22 22:51:01 +0200

    feat: Adding default values in the Email details window and rendering email message on webview.

--- src/com/zubaku/controllers/EmailDetailsController.java
+++ src/com/zubaku/controllers/EmailDetailsController.java
@@ -1,13 +1,18 @@
 package com.zubaku.controllers;
 
+import com.zubaku.models.EmailMessage;
 import com.zubaku.processors.EmailProcessor;
 import com.zubaku.processors.ViewProcessor;
+import com.zubaku.services.MessageRendererService;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.web.WebView;
 
-public class EmailDetailsController extends BaseController {
+public class EmailDetailsController extends BaseController implements Initializable {
   @FXML private Label attachmentsLabel;
 
   @FXML private HBox downloadsHBox;
@@ -21,5 +26,18 @@ public class EmailDetailsController extends BaseController {
   public EmailDetailsController(
       EmailProcessor emailProcessor, ViewProcessor viewProcessor, String fxmlName) {
     super(emailProcessor, viewProcessor, fxmlName);
+  }
+
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    EmailMessage emailMessage = emailProcessor.getSelectedEmailMessage();
+
+    subjectLabel.setText(emailMessage.getSubject());
+    senderLabel.setText(emailMessage.getSender());
+
+    MessageRendererService service = new MessageRendererService(webView.getEngine());
+    service.setEmailMessage(emailMessage);
+    service.restart();
   }
 }
