7f4cb74f2af4f3713a0d74061231256c08969895
ArditZubaku <zubaku92@gmail.com>
2023-10-24 23:09:32 +0200

    feat: Saving credentials to persistence on program exit.

--- src/com/zubaku/application/Launcher.java
+++ src/com/zubaku/application/Launcher.java
@@ -1,12 +1,20 @@
 package com.zubaku.application;
 
+import com.zubaku.models.EmailAccount;
+import com.zubaku.persistence.PersistenceAccess;
+import com.zubaku.persistence.ValidAccount;
 import com.zubaku.processors.EmailProcessor;
 import com.zubaku.processors.ViewProcessor;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.application.Application;
 import javafx.application.HostServices;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
+  private PersistenceAccess persistenceAccess = new PersistenceAccess();
+  private EmailProcessor emailProcessor = new EmailProcessor();
+
   public static void main(String[] args) {
     launch(args);
   }
@@ -15,8 +23,19 @@ public class Launcher extends Application {
   public void start(Stage stage) throws Exception {
 
     HostServices hostServices = getHostServices();
-    ViewProcessor viewProcessor = new ViewProcessor(new EmailProcessor(), hostServices);
+    ViewProcessor viewProcessor = new ViewProcessor(emailProcessor, hostServices);
     viewProcessor.showLoginWindow();
     viewProcessor.updateStyles();
+  }
+
+  @Override
+  public void stop() throws Exception {
+    List<ValidAccount> validAccounts = new ArrayList<>();
+
+    for (EmailAccount emailAccount : emailProcessor.getEmailAccounts()) {
+      validAccounts.add(new ValidAccount(emailAccount.getEmail(), emailAccount.getPassword()));
+    }
+
+    persistenceAccess.saveToPersistence(validAccounts);
   }
 }
